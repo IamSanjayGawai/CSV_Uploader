d424911f2e4539e090eec60cc4ac4443151f37a9
Rambo <sanjaygawai1002@gmail.com>
2023-05-05 14:39:53 +0530

    added

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const PORT = 8000;
+const app = express();
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(expressLayouts);
+
+//ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('./assets'));
+
+
+
+
+
+//routes
+app.use('/', require('./routes/csvRoutes'));
+
+//server listening
+app.listen(PORT, (err) => {
+    if(err) console.log("error listening on", PORT);
+
+    console.log('listening on port', PORT);
+})
